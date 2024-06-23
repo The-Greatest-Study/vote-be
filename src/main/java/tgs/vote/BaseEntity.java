963ed35d715f2b3c4da09686b19eda0579803464
JiHongKim15 <15computer@hanmail.net>
2024-06-23 23:25:04 +0900

    add: BaseEntity 추가

---
+++ src/main/java/tgs/vote/BaseEntity.java
@@ -0,0 +1,25 @@
+package tgs.vote;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Instant updatedAt;
+
+}
