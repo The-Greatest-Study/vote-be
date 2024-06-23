235c32f9a413323de0ea5cd4e3f4d24e9f43bf7a
coolcool <coolcool.with.you@gmail.com>
2024-06-23 18:25:03 +0900

    ADD - supabase, jpa 설정 추가

--- src/main/java/tgs/vote/vote/entity/VoteEntity.java
+++ src/main/java/tgs/vote/vote/entity/VoteEntity.java
@@ -2,18 +2,22 @@ package tgs.vote.vote.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
-@Entity
+@Getter
+@Entity(name = "tb_vote")
 public class VoteEntity {
-
-    @Id
-    private Long id;
-    private String title;
-    private String description;
-    private String startDate;
-    private String endDate;
-    private String creator;
-    private String thumbnail;
-    private String status;
-
+    @Id private Long vote_id;
+    private String vote_title;
+    private String vote_description;
+    private String vote_thumbnail;
+    private String vote_creator;
+    private String vote_open_status;
+    private String vote_anonymous_available_yn;
+    private String vote_in_process_result_open_status;
+    private String vote_end_result_open_status;
+    private String vote_again_available_yn;
+    private String vote_create_datetime;
+    private String vote_start_datetime;
+    private String vote_end_datetime;
 }
--- src/main/java/tgs/vote/vote/service/VoteService.java
+++ src/main/java/tgs/vote/vote/service/VoteService.java
@@ -1,21 +1,18 @@
 package tgs.vote.vote.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tgs.vote.vote.entity.VoteEntity;
 import tgs.vote.vote.repository.VoteRepository;
 
-import java.time.LocalDate;
-import java.util.List;
-
-
-@Service
 @RequiredArgsConstructor
+@Service
 public class VoteService {
-
     private final VoteRepository voteRepository;
 
-    public List<VoteEntity> retrieveOngoingVoteList() {
-        return voteRepository.findOngoingVotes(LocalDate.now());
+    public List<VoteEntity> getVoteList() {
+        List<VoteEntity> voteEntityList = voteRepository.findByVoteCreator("CoolCool");
+        return voteEntityList;
     }
 }
