015548030690d7bc549bde349a001faa347022a8
JiHongKim15 <15computer@hanmail.net>
2024-07-28 19:34:32 +0900

    fix: package change

--- src/main/java/tgs/vote/adapter/out/repository/UserJpaRepository.java
+++ src/main/java/tgs/vote/adapter/out/repository/UserJpaRepository.java
@@ -1,4 +1,4 @@
-package tgs.vote.adapter.out;
+package tgs.vote.adapter.out.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
