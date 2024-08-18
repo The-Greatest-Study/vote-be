2f64a083a5e7ceb592ee4c8d62186d45d66d89a8
coolcool <coolcool.with.you@gmail.com>
2024-08-18 15:24:34 +0900

    UPDATE - Vote save method 수정 (어떻게 저장되는지를 VoteService에서 알 필요가 없기 때문에 VoteAdapter로 로직 이동)

--- src/main/java/tgs/vote/adapter/out/VoteAdapter.java
+++ src/main/java/tgs/vote/adapter/out/VoteAdapter.java
@@ -3,25 +3,49 @@ package tgs.vote.adapter.out;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import tgs.vote.adapter.mapper.ChoiceMapper;
+import tgs.vote.adapter.mapper.QuestionMapper;
 import tgs.vote.adapter.mapper.VoteMapper;
+import tgs.vote.adapter.model.persistence.vote.ChoiceEntity;
+import tgs.vote.adapter.model.persistence.vote.QuestionEntity;
 import tgs.vote.adapter.model.persistence.vote.VoteEntity;
+import tgs.vote.adapter.out.persistance.ChoiceJpaRepository;
+import tgs.vote.adapter.out.persistance.QuestionJpaRepository;
 import tgs.vote.adapter.out.persistance.VoteJpaRepository;
 import tgs.vote.application.out.VotePort;
+import tgs.vote.domain.vote.Choice;
+import tgs.vote.domain.vote.Question;
 import tgs.vote.domain.vote.Vote;
 
 @Component
 @RequiredArgsConstructor
 public class VoteAdapter implements VotePort {
     private final VoteJpaRepository voteJpaRepository;
+    private final QuestionJpaRepository questionJpaRepository;
+    private final ChoiceJpaRepository choiceJpaRepository;
     private final VoteMapper voteMapper;
+    private final QuestionMapper questionMapper;
+    private final ChoiceMapper choiceMapper;
 
     @Override
     public Vote save(Vote vote) {
-        VoteEntity entity = voteMapper.toVoteEntity(vote);
+        VoteEntity voteEntity = voteMapper.toVoteEntity(vote);
 
-        VoteEntity savedEntity = voteJpaRepository.save(entity);
+        VoteEntity savedVoteEntity = voteJpaRepository.save(voteEntity);
 
-        return voteMapper.toVote(savedEntity);
+        for (Question question : vote.getQuestionList()) {
+            question.setVoteId(savedVoteEntity.getVoteId());
+            QuestionEntity questionEntity = questionMapper.toQuestionEntity(question);
+            QuestionEntity savedQuestionEntity = questionJpaRepository.save(questionEntity);
+
+            for (Choice choice : question.getQuestionChoiceList()) {
+                choice.setQuestionId(savedQuestionEntity.getQuestionId());
+                ChoiceEntity choiceEntity = choiceMapper.toChoiceEntity(choice);
+                choiceJpaRepository.save(choiceEntity);
+            }
+        }
+
+        return voteMapper.toVote(savedVoteEntity);
     }
 
     @Override
